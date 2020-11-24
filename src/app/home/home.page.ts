7fb7eb933037c3181ddcccedbbd9f7c4db2257ff
sebastian gomez <mezgojobichisempay@gmail.com>
2020-11-24 11:14:53 -0500

    medio punto2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NoticiasService } from '../services/noticias.service';
+
 interface SectionsI {
   header: {
     title: string
@@ -23,32 +23,31 @@ export class HomePage implements OnInit {
     imgSize: 3,
     contentSize: 9
   }
-  
-  sections: Array<SectionsI> = this.noticiasService.getAll();
-  // sections: Array<SectionsI> = [
-  //   {
-  //     header: {
-  //       title: "Noticias"
-  //     },
-  //     body: {
-  //       img: "https://picsum.photos/200/300?random=1",
-  //       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum cumque laudantium accusantium fugiat saepe cupiditate perspiciatis commodi quod tempore illo doloribus mollitia et veniam debitis sequi accusamus vitae! Laudantium.",
-  //       imgSize: 3,
-  //       contentSize: 9
-  //     }
-  //   },
-  //   {
-  //     header: {
-  //       title: "Salud"
-  //     },
-  //     body: {
-  //       img: "https://picsum.photos/200/300?random=2",
-  //       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum cumque laudantium accusantium fugiat saepe cupiditate perspiciatis commodi quod tempore illo doloribus mollitia et veniam debitis sequi accusamus vitae! Laudantium.",
-  //     }
-  //   }
-  // ]
 
-  constructor( private noticiasService: NoticiasService ) { }
+  sections: Array<SectionsI> = [
+    {
+      header: {
+        title: "Noticias"
+      },
+      body: {
+        img: "https://picsum.photos/200/300?random=1",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum cumque laudantium accusantium fugiat saepe cupiditate perspiciatis commodi quod tempore illo doloribus mollitia et veniam debitis sequi accusamus vitae! Laudantium.",
+        imgSize: 3,
+        contentSize: 9
+      }
+    },
+    {
+      header: {
+        title: "Salud"
+      },
+      body: {
+        img: "https://picsum.photos/200/300?random=2",
+        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, earum cumque laudantium accusantium fugiat saepe cupiditate perspiciatis commodi quod tempore illo doloribus mollitia et veniam debitis sequi accusamus vitae! Laudantium.",
+      }
+    }
+  ]
+
+  constructor() { }
 
   ngOnInit() {
   }
